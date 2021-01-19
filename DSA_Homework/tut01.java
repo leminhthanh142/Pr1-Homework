4b0969b23eaf53687db50a48f06e9d87090d9050
leminhthanh142 <thanhle12345789@gmail.com>
2021-01-19 22:37:17 +0700

    updated

--- DSA_Homework/tut01.java
+++ DSA_Homework/tut01.java
@@ -5,25 +5,42 @@ import java.util.Scanner;
 public class tut01 {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+
+        // exercise 1
         System.out.print("Enter your name: ");
         String userName = input.nextLine();
         System.out.println("Result: " + characterCount(userName) + " characters");
         System.out.println();
 
+
+        // exercise 2
         System.out.print("How many number you want to add: ");
         int n = input.nextInt();
-        System.out.println("Max value is: " + maxInteger(n));
+        int [] arr= new int[n];
+
+        for (int i=0; i<n; i++)
+            arr[i] = input.nextInt();
+
+        System.out.println("Max value is: " + maxInteger(arr));
         System.out.println();
 
+
+        // exercise 3
         System.out.print("How many number of array: ");
         int num = input.nextInt();
-        int [] arr = sortedArr(num);
+        int [] arr1 = new int[num];
+        for (int i=0; i<num; i++)
+            arr1[i] = input.nextInt();
+
+        int [] arr2 = sortedArr(arr1);
         System.out.print("Sorted array: ");
-        for (int i : arr) {
+        for (int i : arr2) {
             System.out.print(i + " ");
         }
         System.out.println();
 
+
+        // exercise 4
         System.out.print("How many name you want to add: ");
         int numOfNames = input.nextInt();
         input.nextLine();
@@ -39,6 +56,8 @@ public class tut01 {
 
     }
 
+
+    // exercise 1
     public static int characterCount(String name) {
         int count = 0;
         for (int i=0; i<name.length(); i++) {
@@ -47,38 +66,37 @@ public class tut01 {
         return count;
     }
 
-    public static int maxInteger (int n) {
-        int [] arr = new int[n];
+
+    // exercise 2
+    public static int maxInteger (int [] arr) {
         int max = arr[0];
 
-        for (int i=0; i<arr.length; i++) {
-            arr[i] = input.nextInt();
-            if (arr[i] > max) max = arr[i];
-        }
+        for (int j : arr)
+            if (j > max) max = j;
 
         return max;
     }
 
-    public static int[] sortedArr (int n) {
-        int [] arr = new int[n];
-        int temp;
 
-        for (int i=0; i<arr.length; i++) {
-            arr[i] = input.nextInt();
-        }
+    // exercise 3
+    public static int[] sortedArr (int [] arr) {
 
+        // selection sort algorithm
         for (int i=0; i<arr.length - 1; i++) {
-            for (int j=i+1; j<arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+            int minIndex = i;
+            for (int j=i+1; j<arr.length; j++)
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
         return arr;
     }
 
+
+    // exercise 4
     public static int nameSearching (String [] names, String name) {
         for (int i=0; i<names.length; i++) {
             if (name.equals(names[i])) return i;
